ad96894046543dba54f63eec5693ccbbd166472b
sleep3r <sleep3r@icloud.com>
2024-05-29 21:59:13 +0300

    Init

---
+++ Makefile
@@ -0,0 +1,21 @@
+APP_NAME=sleep3r/anycv
+CONTAINER_NAME=anycv
+PROJECT_NAME=/anycv
+
+.PHONY: format test train install help
+
+install:  ## Install the requirements
+	pip install -e .
+
+train:  ## Train the model
+	PYTHONPATH=$PWD:$PYTHONPATH mim train $(PROJECT) $(CONFIG) --gpus $(GPUS) --work-dir $(WORKDIR)
+
+test:  ## Run the tests
+	pytest -s tests
+
+format:  ## Format configs
+	@isort ./configs
+	@black ./configs
+
+help: ## This help.
+	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
---
+++ configs/effnet.py
@@ -0,0 +1,84 @@
+PROJECT_DIR = "/Users/alexander/Desktop/anycv/"
+
+_base_ = [
+    'mmlab/mmpretrain/_base_/models/efficientnet_b0.py',
+    'mmlab/mmpretrain/_base_/default_runtime.py',
+]
+
+for i, item in enumerate(_base_):
+    _base_[i] = PROJECT_DIR + item
+
+
+# Model settings
+model = dict(
+    head=dict(
+        num_classes=2,
+        topk=(1,),
+    ),
+)
+
+# dataset settings
+data_preprocessor = dict(
+    mean=[127.5, 127.5, 127.5],
+    std=[127.5, 127.5, 127.5],
+    # convert image from BGR to RGB
+    to_rgb=True,
+)
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', scale=224),  # Resize the image to 224x224
+    dict(type='Normalize', **data_preprocessor),  # Normalizing the images
+    dict(type='PackInputs'),
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', scale=224),  # Resize the image to 224x224
+    dict(type='Normalize', **data_preprocessor),  # Normalizing the images
+    dict(type='PackInputs'),
+]
+
+train_dataloader = dict(
+    batch_size=8,  # Adjust batch size as per your system capabilities
+    shuffle=True,
+    dataset=dict(
+        type='CustomDataset',
+        data_prefix='/Users/alexander/Desktop/image_classification.train/data/train',
+        with_label=True,
+        pipeline=train_pipeline,
+    ),
+    num_workers=8,  # Adjust num_workers as per your system capabilities
+)
+
+val_dataloader = dict(
+    batch_size=8,  # Adjust batch size as per your system capabilities
+    shuffle=False,
+    dataset=dict(
+        type='CustomDataset',
+        data_prefix='/Users/alexander/Desktop/image_classification.train/data/val',
+        with_label=True,
+        pipeline=test_pipeline,
+    ),
+    num_workers=8,  # Adjust num_workers as per your system capabilities
+)
+
+
+# Training settings
+train_cfg = dict(
+    by_epoch=True,
+    max_epochs=50,  # Increase the number of epochs for better training
+    val_interval=2,
+)
+val_cfg = dict()
+val_evaluator = dict(type='Accuracy', topk=(1,))
+
+# Testing settings
+test_cfg = None
+
+optim_wrapper = dict(
+    # Use SGD optimizer to optimize parameters.
+    optimizer=dict(type='Adam', lr=0.001, weight_decay=0.0001)
+)
+
+load_from = "https://download.openmmlab.com/mmclassification/v0/efficientnet/efficientnet-b0_3rdparty_8xb32_in1k_20220119-a7e2a0b1.pth"
